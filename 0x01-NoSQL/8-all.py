084cc2ed374a5fe0fff3cb1f34c27d4f1316348e
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-12-19 19:11:52 +0100

    Remove if __name__ == __main__

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,10 +2,9 @@
 """This module contains one function"""
 
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """returns a list all documents in a collection"""
-        result = mongo_collection.find()
-        if not result:
-            return []
-        return result
+def list_all(mongo_collection):
+    """returns a list all documents in a collection"""
+    result = mongo_collection.find()
+    if not result:
+        return []
+    return result
